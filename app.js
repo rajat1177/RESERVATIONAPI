45db3bcd8fc50d597a18e2ac0bc2a9b24d77616d
rajat1177 <rajat.kumargzb1177@gmail.com>
2024-06-23 15:34:38 +0530

    second commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import reservationRouter from "./routes/reservationRouter.js"
 dotenv.config({path:"./config/.env"})
 const app=express();
 app.use(cors({
-    origin:process.env.FRONTEND , // Replace with your frontend origin
+    origin:process.env.FRONTENDURL , // Replace with your frontend origin
     credentials: true, // Enable credentials (cookies, authorization headers, etc.)
   }));
 app.use(express.json());
